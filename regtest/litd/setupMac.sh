e0924b1db14f566483ddee68050b5ca2e6f48e0a
Nicholas Sandomeno <nsando61@gmail.com>
2024-06-27 17:40:46 -0400

    where is admin macaroon and tapd start data?

--- regtest/litd/setupMac.sh
+++ regtest/litd/setupMac.sh
@@ -25,7 +25,7 @@ sudo docker exec -it -u bitcoin bitcoind-backend bitcoin-cli -regtest -rpcpasswo
 # setup user
 user=`sudo docker exec -it user-litd lncli --network=regtest --rpcserver=localhost:10010 newaddress p2tr`
 user=$(echo $user | awk -F'"' '{print $4}')
-echo "bob address => $user"
+echo "user address => $user"
 
 sudo docker exec -it -u bitcoin bitcoind-backend bitcoin-cli -regtest -rpcpassword=polarpass -rpcuser=polaruser -rpcwallet=miningwallet sendtoaddress $user 200
 sudo docker exec -it -u bitcoin bitcoind-backend bitcoin-cli -regtest -rpcpassword=polarpass -rpcuser=polaruser -rpcwallet=miningwallet -generate 6
@@ -41,9 +41,9 @@ if [[ "$existing_wallet" == "miningwallet" ]]; then
   echo "======= lookup group key for lnfi ========"
   group_key_lnfi=$(docker exec -it lnfi-litd tapcli -n regtest assets groups | jq '.groups | map_values(select(.assets[] | .tag == "lnfi")) | keys[0]')
   echo $group_key_lnfi
-  echo "======= lookup group key for usercoin ========"
-  group_key_user=$(docker exec -it user-litd tapcli -n regtest --rpcserver=localhost:10030 assets groups | jq '.groups | map_values(select(.assets[] | .tag == "usercoin")) | keys[0]')
-  echo $group_key_user
+#   echo "======= lookup group key for usercoin ========"
+#   group_key_user=$(docker exec -it user-litd tapcli -n regtest --rpcserver=localhost:10030 assets groups | jq '.groups | map_values(select(.assets[] | .tag == "usercoin")) | keys[0]')
+#   echo $group_key_user
 else
   echo "mining wallet not found, creating new wallet"
   echo "======== lnfi mints assets ========="
@@ -56,11 +56,34 @@ else
   sleep 3
 
   echo "======== syncing user to lnfi universe ========="
-  sudo docker exec -it user-litd tapcli --rpcserver=localhost:10030 --network=regtest universe federation add --universe_host=lnfi-litd:10029
-  sudo docker exec -it user-litd tapcli --rpcserver=localhost:10030 --network=regtest universe federation config global --proof_type issuance --allow_insert true
-  sudo docker exec -it user-litd tapcli --rpcserver=localhost:10030 --network=regtest universe federation config global --proof_type transfer --allow_insert true
+  sudo docker exec -it user-litd tapcli --rpcserver=localhost:10030 --network=regtest --tapddir=/root/.litd/tapd universe federation add --universe_host=lnfi-litd:10029
+  sudo docker exec -it user-litd tapcli --rpcserver=localhost:10030 --network=regtest --tapddir=/root/.litd/tapd universe federation config global --proof_type issuance --allow_insert true
+  sudo docker exec -it user-litd tapcli --rpcserver=localhost:10030 --network=regtest --tapddir=/root/.litd/tapd universe federation config global --proof_type transfer --allow_insert true
 
-  sudo docker exec -it user-litd tapcli --rpcserver=localhost:10030 --network=regtest universe sync --universe_host=lnfi-litd:10029
+  sudo docker exec -it user-litd tapcli --rpcserver=localhost:10030 --network=regtest --tapddir=/root/.litd/tapd universe sync --universe_host=lnfi-litd:10029
+  
+#   echo "======== sending lnfi to user ========="
+#   sudo docker exec -it user-litd tapcli --rpcserver=localhost:10030 --network=regtest --tapddir=/root/.litd/tapd addrs new --asset_id
 fi
 
 
+echo "======== lnd tls cert hex ========="
+alice_cert=$(sudo docker exec -it lnfi-litd cat /root/.litd/lnd/tls.cert | xxd -p -c 1000 | tr -d '\n')
+echo "alice cert => $alice_cert"
+
+echo "======== lnd admin macaroon hex ========="
+alice_macaroon=$(sudo docker exec -it lnfi-litd xxd -p -c 1000 /root/.litd/lnd/data/chain/bitcoin/regtest/admin.macaroon | tr -d '\n')
+echo "alice macaroon => $alice_macaroon"
+
+# echo "======== tapd tls cert hex ========="
+# tapd_cert=$(sudo docker exec -it lnfi-litd cat /root/.litd/tapd/tls.cert | xxd -p -c 1000 | tr -d '\n')
+# echo "tapd cert => $tapd_cert"
+
+# echo "======== tapd admin macaroon hex ========="
+# tapd_macaroon=$(sudo docker exec -it lnfi-litd xxd -p -c 1000 /root/.litd/tapd/data/regtest/admin.macaroon | tr -d '\n')
+# echo "tapd macaroon => $tapd_macaroon"
+
+# start auto mining
+sudo docker exec -d -u bitcoin bitcoind-backend bash -c "/home/bitcoin/mining.sh"
+echo "finish start auto mining"
+sleep 3
